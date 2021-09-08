cd6ea6701c1ec440cbd8b331a6a6b5df6170fe19
zfliu <zfliup@163.com>
2021-09-08 10:23:05 +0800

    create

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
 import infiniteScroll from 'vue-infinite-scroll'
 
 Vue.use(infiniteScroll)
