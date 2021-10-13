0eb7abdfd6e9c4e15d85a6c344e18c7b755f471e
zfliu <zfliup@163.com>
2021-10-13 10:08:35 +0800

    hjh

--- src/views/viewPage/Study.vue
+++ src/views/viewPage/Study.vue
@@ -1,36 +1,36 @@
 <template>
-    <div class="study" @click="add">
-        {{ fn }}
-    </div>
+  <div class="study" @click="add">
+    {{ fn }}44446575687
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Study',
-    data() {
-        return {
-            msg: 1
-        }
-    },
-    computed: {
-        fn: function () {
-            return this.msg + 3
-        }
-    },
-    watch: {},
-    mounted() {
-
-    },
-    methods: {
-        add() {
-            this.msg++
-        }
+  name: 'Study',
+  data() {
+    return {
+      msg: 1
     }
+  },
+  computed: {
+    fn: function () {
+      return this.msg + 3
+    }
+  },
+  watch: {},
+  mounted() {
+
+  },
+  methods: {
+    add() {
+      this.msg++
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .study {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
--- src/views/viewPage/Test.vue
+++ src/views/viewPage/Test.vue
@@ -1,40 +1,40 @@
 <!--vue自定义指令学习-->
 <template>
-    <div>
-        test
-        <div v-focus>{{ test }}</div>
-    </div>
+  <div>
+    tkjshfkj57685875
+    <div v-focus>{{ test }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Test",
-    data() {
-        return {
-            test: 'ww'
-        }
-    },
-    directives: {
-        focus: {
-            // 指令的定义
-            inserted: function (el, binding, vnode, oldVnode) {
-                console.log(el, binding, vnode, oldVnode, "inserted")
-                console.log(el.offsetHeight, el.innerHTML)
-            },
-            bind: function (el, binding, vnode, oldVnode) {
-                console.log(el, binding, vnode, oldVnode, 'bind')
-            },
-            unbind: function (el, binding, vnode, oldVnode) {
-                console.log(el, binding, vnode, oldVnode, 'unbind')
-            },
-            componentUpdated: function (el, binding, vnode, oldVnode) {
-                console.log(el, binding, vnode, oldVnode, 'componentUpdated')
-            },
-            update: function (el, binding, vnode, oldVnode) {
-                console.log(el, binding, vnode, oldVnode, 'update')
-            }
-        }
+  name: "Test",
+  data() {
+    return {
+      test: 'ww'
     }
+  },
+  directives: {
+    focus: {
+      // 指令的定义
+      inserted: function (el, binding, vnode, oldVnode) {
+        console.log(el, binding, vnode, oldVnode, "inserted")
+        console.log(el.offsetHeight, el.innerHTML)
+      },
+      bind: function (el, binding, vnode, oldVnode) {
+        console.log(el, binding, vnode, oldVnode, 'bind')
+      },
+      unbind: function (el, binding, vnode, oldVnode) {
+        console.log(el, binding, vnode, oldVnode, 'unbind')
+      },
+      componentUpdated: function (el, binding, vnode, oldVnode) {
+        console.log(el, binding, vnode, oldVnode, 'componentUpdated')
+      },
+      update: function (el, binding, vnode, oldVnode) {
+        console.log(el, binding, vnode, oldVnode, 'update')
+      }
+    }
+  }
 
 }
 </script>
